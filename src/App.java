16866e026adb8189385c1127080dc4e7237a4f8e
krishis2003 <you@example.com>
2022-04-23 20:48:08 +0300

    irir

--- src/App.java
+++ src/App.java
@@ -1,9 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
-        System.out.println("asdadfaf");
+        JButton but;
+        JFrame frame = new JFrame("Autoplaca vadibas panelis profesionaliem");
+        JTextField textfield;  
+     textfield = new JTextField("kalkulatora sadala");
+     textfield.setBounds(50, 150, 200, 30);
+     textfield.setLocation(20,20);
+     frame.add(textfield);
+    
+     frame.setSize(800, 800);
+     frame.setLayout(null);
+     frame.setVisible(true);
     }
 }
