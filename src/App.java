ac070cc5c4800729df6dec0ccbcc74d004ff8f7e
krishis2003 <you@example.com>
2022-04-29 18:08:38 +0300

    plus

--- src/App.java
+++ src/App.java
@@ -1,9 +1,19 @@
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import java.awt.event.*;
+
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        double first;
+        double second;
+        double result;
+        String operation;
+        String answer;
+
         JButton but;
         JButton but0;
         JButton but2;
@@ -21,23 +31,124 @@ public class App {
         JButton butkomats;
         JButton butzimmaina;
         JFrame frame = new JFrame("Autoplaca vadibas panelis profesionaliem");
-        JTextField textfield;  
+        JTextField textfield; 
+        textfield = new JTextField(" ");
+        textfield.setBounds(50, 150, 200, 30);
+        textfield.setLocation(20,20);
+        frame.add(textfield);
+        
         butminus = new JButton("-");
+        butminus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+butminus.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but = new JButton("1");
+        but.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but0 = new JButton("0");
+        but0.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+           {
+                        String nummber = textfield.getText()+but0.getText();
+                        textfield.setText(nummber);
+                    }
+                ; 
+            }
+        }); 
         but2 = new JButton("2");
+        but2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but2.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but3 = new JButton("3");
+        but3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but3.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but4 = new JButton("4");
+        but4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but4.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but5 = new JButton("5");
+        but5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but5.getText();
+                        textfield.setText(nummber);
+            }
+        }); 
         but6 = new JButton("6");
+        but6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but6.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but7 = new JButton("7");
+        but7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but7.getText();
+                        textfield.setText(nummber);
+            }
+        }); 
         but8 = new JButton("8");
+        but8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but8.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         but9 = new JButton("9");
+        but9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                String nummber = textfield.getText()+but9.getText();
+                textfield.setText(nummber);
+            }
+        }); 
         butzimmaina = new JButton("+/-");
+        butzimmaina.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                System.out.println("You clicked the button zimmaina");
+            }
+        }); 
         butkomats = new JButton(".");
+        butkomats.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                System.out.println("You clicked the button komats");
+            }
+        }); 
         butreiz = new JButton("×");
+        butreiz.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                System.out.println("You clicked the button reiz");
+            }
+        }); 
         butdal = new JButton(":");
+        butdal.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+                System.out.println("You clicked the button dal");
+            }
+        }); 
         butplus = new JButton("+");
+        butplus.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e)            {
+               Double first = Double.parseDouble(textfield.getText());
+                textfield.setText("");
+                String operation ="+";
+            }
+        }); 
         butzimmaina.setBounds(50,50,50,50);
         butminus.setBounds(50,50,50,50);
         butkomats.setBounds(50,50,50,50);
@@ -86,11 +197,6 @@ public class App {
         frame.add(butplus);
         frame.add(butminus);
         frame.add(butzimmaina);
-     textfield = new JTextField("kalkulatora sadala");
-     textfield.setBounds(50, 150, 200, 30);
-     textfield.setLocation(20,20);
-     frame.add(textfield);
-    
      frame.setSize(800, 800);
      frame.setLayout(null);
      frame.setVisible(true);
